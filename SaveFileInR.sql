7bffbfe8c05559ae70913ccc2f8a666430196ac3
Vladimír Mužný <dropman@dropman.cz>
2018-08-28 14:33:38 +0200

    A parameter added to the write.csv
    The parameter eliminates row names in saved CSV

--- SaveFileInR.sql
+++ SaveFileInR.sql
@@ -1,4 +1,4 @@
 exec sp_execute_external_script
 	@language = N'R'
-	, @script = N'write.csv(InputDataSet, "C:\\temp\\test.csv");'
+	, @script = N'write.csv(InputDataSet, "C:\\temp\\test.csv", row.names = FALSE);'
 	, @input_data_1 = N'SELECT ProductId, Name FROM Production.Product'
